c5a619317ad3698c686bb2d86622f99c91a74fc4
Peter Nguyen <peter@phn.sh>
2024-02-10 20:54:01 -0600

    Use webcrypto to generate dkim

--- scripts/reset-dkim.js
+++ scripts/reset-dkim.js
@@ -1,30 +1,32 @@
 import { spawnSync } from 'node:child_process';
-import { generateKeyPairSync } from 'node:crypto';
+import { webcrypto } from 'node:crypto';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { readFileSync } from 'node:fs';
-import toml from 'toml'
+import toml from 'toml';
 
-const { publicKey, privateKey } = generateKeyPairSync('rsa', {
-	modulusLength: 2048,
-	publicKeyEncoding: {
-		type: 'spki',
-		format: 'der',
+const { publicKey, privateKey } = await webcrypto.subtle.generateKey(
+	{
+		name: 'RSASSA-PKCS1-v1_5',
+		modulusLength: 2048,
+		publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+		hash: 'SHA-256'
 	},
-	privateKeyEncoding: {
-		type: 'pkcs8',
-		format: 'der',
-	},
-});
+	true,
+	['sign', 'verify']
+);
 
-const dkimPublicKey = publicKey.toString('base64');
-const dkimPrivateKey = privateKey.toString('base64');
+const publicKeyBuffer = await webcrypto.subtle.exportKey('spki', publicKey);
+const privateKeyBuffer = await webcrypto.subtle.exportKey('pkcs8', privateKey);
+
+const dkimPublicKey = Buffer.from(publicKeyBuffer).toString('base64');
+const dkimPrivateKey = Buffer.from(privateKeyBuffer).toString('base64');
 
 const wranglerTomlPath = resolve(dirname(fileURLToPath(import.meta.url)), '..', 'wrangler.toml');
 const wranglerToml = toml.parse(readFileSync(wranglerTomlPath, { encoding: 'utf-8' }));
 
-const dkimDomain = wranglerToml['vars']['DKIM_DOMAIN']
-const dkimSelector = wranglerToml['vars']['DKIM_SELECTOR']
+const dkimDomain = wranglerToml['vars']['DKIM_DOMAIN'];
+const dkimSelector = wranglerToml['vars']['DKIM_SELECTOR'];
 
 const wrangler = spawnSync('npm', ['exec', '--', 'wrangler', 'secret', 'put', 'DKIM_PRIVATE_KEY'], {
 	input: dkimPrivateKey,
@@ -35,7 +37,7 @@ if (wrangler.status !== 0) {
 	process.exit(wrangler.status);
 }
 
-console.log(`Worker DKIM_PRIVATE_KEY has been updated.`)
-console.log(`Add this DNS record to '${dkimDomain}':\n`)
+console.log(`Worker DKIM_PRIVATE_KEY has been updated.`);
+console.log(`Add this DNS record to '${dkimDomain}':\n`);
 
 console.log(`${dkimSelector}._domainkey IN TXT "v=DKIM1; k=rsa; p=${dkimPublicKey}"`);
